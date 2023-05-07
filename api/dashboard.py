77b909e11365401afefb0d7215fd4c36f8a411cb
Dishyma <marojas4126@gmail.com>
2023-05-06 22:19:10 -0500

    Finish dashboard, and create sh file

--- api/dashboard.py
+++ api/dashboard.py
@@ -1,24 +1,36 @@
+from doctest import OutputChecker
+from tkinter.tix import InputOnly
 import pandas as pd
-import ploty.graph_objects as go
+import plotly.graph_objects as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc as dcc
+from dash import html as html
 
 url = "http://siata.gov.co:8089/estacionesNivel/cf7bb09b4d7d859a2840e22c3f3a9a8039917cc3/"
-data = pd.read_json(url, convert_dates = 'True')
+data = pd.read_json(url, convert_dates=True)
+
 latr = []
 lonr = []
 zr = []
-for i in range(0,100):
+for i in range(0, 100):
     zr.append(data['datos'][i]['porcentajeNivel'])
     latr.append(data['datos'][i]['coordenadas'][0]['latitud'])
     lonr.append(data['datos'][i]['coordenadas'][0]['longitud'])
-fig = go.Figure(go.Densitymapbox(lat = latr, lon = lonr, z = zr, radius = 20, opacity = 0.9, zmin = 0, zmax = 100))
-fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon = -75.589, mapbox_center_lat = 6.2429)
-fig.update_layout(margin={"r" : 0, "t" : 0, "l" : 0, "b" : 0})
-app = dash.Dash()
+
+fig = go.Figure(go.Densitymapbox(lat=latr, lon=lonr, z=zr, radius=20, opacity=0.9, zmin=0, zmax=100))
+fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=-75.589, mapbox_center_lat=6.2429)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+app = dash.Dash(__name__)
+server = app.server  # Create a Flask server instead of the default Dash server
+
 app.layout = html.Div([
-                        html.H1("Este es un proyecto"),
-                        dcc.Graph(figure=fig)
-                        ])
-app.run_server(host='0.0.0.0', port=5000)
+    html.H1("Este es un proyecto"),
+    dcc.Graph(figure=fig),
+])
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', port=5001)
---
+++ inicio.sh
@@ -0,0 +1,9 @@
+##Update repository
+sudo apt update
+
+##Download docker compose
+sudo apt install -y docker-compose
+
+##Pull images
+sudo docker pull ubuntu:latest
+sudo docker pull mysql:latest
